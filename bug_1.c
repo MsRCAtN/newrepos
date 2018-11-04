5b4ef7e33a3e21acdd54cd990a3b6b71e134a930
MsRCAtN <sa46l112@otmail.com>
2018-11-04 20:07:57 +0800

    fuck blu

---
+++ bug_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int 
+int main()
+{
+    for (int i = 2; i != 1; i++)
+    if( i % 2 == 0)
+        printf("baka blu\n");
+    else
+        printf("blu baka\n");
+    return 0;
+}
